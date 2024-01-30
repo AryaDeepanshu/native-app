2e7fa5ed5ae10eb659f385acdf69792cb8e32375
Deepanshu Arya <deepanshu.da85@gmail.com>
2024-01-30 23:10:22 +0530

    yo

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+const {useNavigation} = require('@react-navigation/native');
+import Home from './Home';
+import Settings from './Settings';
+import { StyleSheet, Text, View ,Button } from 'react-native';
+
+const Drawer = createDrawerNavigator();
+const nav = useNavigation();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+    
+    <View style={style.container}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <Button title="Go to Home" onPress={() => nav.navigate('Home')}/>
+    <Button title="Go to Settings" onPress={() => nav.navigate('Settings')}/>
     </View>
+    </>
+    
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
